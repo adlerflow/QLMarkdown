9fb34a74ad77d3b0e829634657cb36839bd4e2de
adlerflow <constantin.adler@icloud.com>
2025-11-01 02:46:44 +0100

    refactor: Remove syntaxhighlight from bridging header
    - Removed #import for syntaxhighlight.h
    - Functions now available via wrapper_highlight.h
    - No functional impact on Swift code
    
    Part of Plan 1: Remove syntaxhighlight.c extension

--- TextDown/TextDown-Bridging-Header.h
+++ TextDown/TextDown-Bridging-Header.h
@@ -7,7 +7,6 @@
 #import "../cmark-gfm/src/cmark-gfm.h"
 #import "../cmark-gfm/extensions/cmark-gfm-core-extensions.h"
 
-#import "../cmark-extra/extensions/syntaxhighlight.h"
 #import "../cmark-extra/extensions/emoji.h"
 #import "../cmark-extra/extensions/inlineimage.h"
 #import "../cmark-extra/extensions/math_ext.h"
